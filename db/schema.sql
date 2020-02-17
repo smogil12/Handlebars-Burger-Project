b8241e0db46d965d1e4ede9f91aeb454f02d172e
Spencer Mogil <smogil12@gmail.com>
2020-02-16 21:28:03 -0500

    error: updated id column in schema but now burger table cannot be found

--- db/schema.sql
+++ db/schema.sql
@@ -4,8 +4,8 @@ create database burgers_db;
 use burgers_db;
 
 CREATE TABLE burgers (
-  item_id INTEGER(100) AUTO_INCREMENT NOT NULL,
+  id INTEGER(100) AUTO_INCREMENT NOT NULL,
   burger_name VARCHAR(100),
   devoured BOOLEAN NOT NULL DEFAULT 0,
-  PRIMARY KEY (item_id)
+  PRIMARY KEY (id)
 );
